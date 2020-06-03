cf30742b65c9f4cdae1e0957e2e244335c0ba4a2
121 <66378153+pbwoioi1@users.noreply.github.com>
2020-06-03 23:42:14 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h2>coba coba</h2>
+    <p>hello mars pleeeeerrrrr</p>
+    <p>world will be destroy coz corona</p>
+  </body>
+</html>
